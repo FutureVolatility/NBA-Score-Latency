bda858ed5f9f8a1ed4904379e695543d948911a3
FutureVolatility <kalebbcooper@gmail.com>
2025-03-07 02:02:12 -0500

    limit-order.py

--- polymarket-examples/trades/limit-order.py
+++ polymarket-examples/trades/limit-order.py
@@ -1,9 +1,10 @@
 import os
 
 from py_clob_client.client import ClobClient
-from py_clob_client.clob_types import ApiCreds, MarketOrderArgs, OrderType
+from py_clob_client.clob_types import ApiCreds, OrderArgs
 from dotenv import load_dotenv
 from py_clob_client.constants import AMOY
+
 from py_clob_client.order_builder.constants import BUY
 
 
@@ -21,14 +22,15 @@ def main():
     chain_id = AMOY
     client = ClobClient(host, key=key, chain_id=chain_id, creds=creds)
 
-    # give token-id from clob, amount in $, and side you would like to execute (buy/sell)
-    order_args = MarketOrderArgs(
-        token_id="example-token-id",
-        amount=100,  # $$$
+    # create limit oder in terms of price, size, side, & token id
+    order_args = OrderArgs(
+        price=0.0005,
+        size=20,
         side=BUY,
+        token_id="example-token",
     )
-    signed_order = client.create_market_order(order_args)
-    resp = client.post_order(signed_order, orderType=OrderType.FOK)
+    signed_order = client.create_order(order_args)
+    resp = client.post_order(signed_order)
     print(resp)
     print("Done!")
 
